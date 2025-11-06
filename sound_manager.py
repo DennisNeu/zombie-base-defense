2d48fa53f081825c259928ed511ee2fb93e4c1ae
Dennis <DennisNeu@users.noreply.github.com>
2025-11-06 12:12:16 +0100

    Add sound for bullet impact

--- sound_manager.py
+++ sound_manager.py
@@ -7,6 +7,8 @@ class SoundManager:
         pygame.mixer.init()
         self.sounds = {
             "gunshot": pygame.mixer.Sound("sfx/gunshot.mp3"),
+            "impact": pygame.mixer.Sound("sfx/impact.mp3"),
+            # Add sound for base hit
         }
         self.sounds["gunshot"].set_volume(0.2)
 

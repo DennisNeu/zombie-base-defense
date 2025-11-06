bce27d5e0d99a4db0cf388638b2c890ac04129e5
Dennis <76845672+DennisNeu@users.noreply.github.com>
2025-11-06 12:05:41 +0100

    Add sound code

---
+++ sound_manager.py
@@ -0,0 +1,15 @@
+"""A class to handle the sound effects in the game."""
+
+import pygame
+
+class SoundManager:
+    def __init__(self):
+        pygame.mixer.init()
+        self.sounds = {
+            "gunshot": pygame.mixer.Sound("sfx/gunshot.mp3"),
+        }
+        self.sounds["gunshot"].set_volume(0.2)
+
+    def play_sound(self, sound_name):
+        if sound_name in self.sounds:
+            self.sounds[sound_name].play()

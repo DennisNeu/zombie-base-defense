8baaf02701eeb2999447c92dfd32d5a79c578f0a
Dennis <DennisNeu@users.noreply.github.com>
2025-11-15 18:43:53 +0100

    Add working menus

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 """A game written in Python using Pygame. It is about a player base which has to defend itself from zombies."""
 
 import pygame
+import pygame_menu as pgm
 import sys
 import random
 import utils
@@ -38,6 +39,49 @@ def spawn_zombie():
     if random.randint(0,60) == 60:  # 2% chance to spawn a zombie each frame
         zombies.append(Zombie(speed=50, health=30))
 
+def start_game():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # left click
+                mx, my = pygame.mouse.get_pos()
+                projectiles.append(Projectile(utils.SCREEN_WITDH / 2, utils.SCREEN_HEIGHT / 2, mx, my))
+                sound_manager.play_sound("gunshot")
+
+        # Game logic goes here
+        for p in projectiles[:]:
+            p.update(dt)
+            if not p.alive:
+                projectiles.remove(p)
+        
+        spawn_zombie()
+        
+        check_collisions()
+
+        if base.get_health() <= 0:
+            running = False  # End game if base health is 0
+            game_over_menu.mainloop(screen)
+
+
+        # Drawing code goes here
+        screen.fill(utils.BACKGROUND_COLOR)  # Clear screen with black
+        base.draw(screen)
+        for b in projectiles:
+            b.draw(screen)
+        for z in zombies:
+            z.draw(screen)
+        
+        # TODO: Refactor text logic
+        base_health_text = font.render(f"Base Health: {base.get_health()}", True, (255, 255, 255), (10, 10, 20))
+        kill_count_text = font.render(f"Kill Count: {utils.kill_count}", True, (255, 255, 255), (10, 50, 20))
+        screen.blit(kill_count_text, (10, 50))
+        screen.blit(base_health_text, (10, 10))
+        pygame.display.flip() # Update the display
+        clock.tick(FPS)
+
+
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((utils.SCREEN_WITDH, utils.SCREEN_HEIGHT))
@@ -48,48 +92,24 @@ dt = clock.tick(60) / 1000
 font = pygame.font.SysFont(None, 36)
 sound_manager = SoundManager()
 
+# Game Over
+game_over_menu = pgm.Menu('Game Over', utils.SCREEN_WITDH, utils.SCREEN_HEIGHT, theme=pgm.themes.THEME_DARK)
+game_over_menu.add.label(f'Final Kill Count: {utils.kill_count}')
+game_over_menu.add.button('Quit', pgm.events.EXIT)
+
 
 base = Base(utils.SCREEN_WITDH / 2 - 25, utils.SCREEN_HEIGHT / 2 - 25, 50, 50)  # Create a base instance
 
 # Loop
-running = True
 projectiles = []
 zombies = []
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # left click
-            mx, my = pygame.mouse.get_pos()
-            projectiles.append(Projectile(utils.SCREEN_WITDH / 2, utils.SCREEN_HEIGHT / 2, mx, my))
-            sound_manager.play_sound("gunshot")
+# Menu
+menu = pgm.Menu('Zombie Defense', utils.SCREEN_WITDH, utils.SCREEN_HEIGHT, theme=pgm.themes.THEME_DARK)
+menu.add.button('Start Game', start_game)
+menu.add.button('Quit', pgm.events.EXIT)
+menu.mainloop(screen)
 
-    # Game logic goes here
-    for p in projectiles[:]:
-        p.update(dt)
-        if not p.alive:
-            projectiles.remove(p)
-    
-    spawn_zombie()
-    
-    check_collisions()
-
-    # Drawing code goes here
-    screen.fill(utils.BACKGROUND_COLOR)  # Clear screen with black
-    base.draw(screen)
-    for b in projectiles:
-        b.draw(screen)
-    for z in zombies:
-        z.draw(screen)
-    
-    # TODO: Refactor text logic
-    base_health_text = font.render(f"Base Health: {base.get_health()}", True, (255, 255, 255), (10, 10, 20))
-    kill_count_text = font.render(f"Kill Count: {utils.kill_count}", True, (255, 255, 255), (10, 50, 20))
-    screen.blit(kill_count_text, (10, 50))
-    screen.blit(base_health_text, (10, 10))
-    pygame.display.flip() # Update the display
-    clock.tick(FPS)
 
 sys.exit()
 pygame.quit()
